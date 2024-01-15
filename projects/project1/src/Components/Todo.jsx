98a17e67a20f30491b4d190ea1a40af8498abc9f
vijayvp09 <vvpillai95@gmail.com>
2024-01-15 16:34:05 +0530

    completed todo project in react (small issue has to be debugged)

--- projects/project1/src/Components/Todo.jsx
+++ projects/project1/src/Components/Todo.jsx
@@ -1,16 +1,43 @@
+import {useState, useRef, useEffect} from 'react'
 import "./CSS/Todo.css"
+import TodoItems from "./TodoItems.jsx"
+
+let count = 0;
 const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+    
+    const add = () => {
+      setTodos([...todos, {no: count++, text: inputRef.current.value, display: ""}]);
+      inputRef.current.value = "";
+      localStorage.setItem("todos_count", count);
+    }
+
+    useEffect(()=>{
+      setTodos([JSON.parse(localStorage.getItem("todos"))]);
+      count = localStorage.getItem("todos_count");
+    }, [])
+
+    useEffect(()=>{
+      setTimeout(()=>{
+        console.log(todos);
+        localStorage.setItem("todos", JSON.stringify(todos));
+      }, 100);
+    }, [todos]) 
+
     return (
     <div className="todo">
-      <div className="todo-header">
+      <div className="todo-header">To-Do List</div>
         <div className="todo-add">
-          <input type="text" placeholder="Add your task" className="todo-input" />
-          <div className="todo-add-btn">Add</div>
+          <input ref={inputRef} type="text" placeholder="Add your task" className="todo-input" />
+          <div  onClick={()=>{add()}} className="todo-add-btn">Add</div>
         </div>
         <div className="todo-list">
-
+          {todos.map((item, index)=>{
+            return <TodoItems key={index} no={item.no} display={item.display} text={item.text} setTodo={setTodos} />
+          })}
         </div>
-      </div>
+      
     </div>
   )
 }
--- projects/project1/src/Components/TodoItems.jsx
+++ projects/project1/src/Components/TodoItems.jsx
@@ -1,8 +1,38 @@
 import "./CSS/TodoItems.css"
-const TodoItems = () => {
-    return (
-        <div>
+import tick from "./Assets/tick.png"
+import not_tick from "./Assets/not_tick.png"
+import cross from "./Assets/cross.png"
 
+const TodoItems = ({no, display, text, setTodo  }) => {
+
+    const deleteTodo = (no) => {
+        let data = JSON.parse(localStorage.getItem("todos"));
+        data = data.filter((todo) => todo.no!==no);
+        setTodo(data);
+    }
+    const toggle = (no) => {
+        let data = JSON.parse(localStorage.getItem("todos"));
+        for(let i = 0;i < data.length; i++){
+            if(data[i].no===no){
+                if (data[i].display===""){
+                    data[i].display = "line-through";
+                }
+                else {
+                    data[i].display="";
+                }
+                break;
+            }
+        }
+        setTodo(data);
+    }
+    return (
+        <div className='todoitems'>
+            <div className={`todoitems-container ${display}`} onClick={()=>{toggle(no)}}>
+                {display===""?<img src={not_tick} alt="" />:<img src={tick} alt="" />}
+                
+                <div className="todoitems-text">{text}</div>
+            </div>
+            <img className="todoitems-cross-icon" onClick={()=>{deleteTodo(no)}} src={cross} alt="" />
         </div>
     )
 }
